fdd714d7416fee663afed5f3c58eb39b5e2d78c9
Omniboy <kachorritou@gmail.com>
2019-06-25 21:32:31 -0300

    limpiarFormulario()

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 let formulario = document.getElementById("form-carga");
 	
 function limpiarFormulario() {
-    // Completar
+    var input_marca = document.getElementsByName('marca');
+    var input_precio = document.getElementsByName('precio');
+    input_marca[0].value = '';
+    input_precio[0].value = '';
 }
 
 function showResults(e) {
