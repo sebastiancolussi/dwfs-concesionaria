ddb7dadcdea648717ea8af736ee4248d72d8c287
Omniboy <kachorritou@gmail.com>
2019-06-25 21:45:30 -0300

    limpiarformulario pull request arreglado

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
 let formulario = document.getElementById("form-carga");
 	
 function limpiarFormulario() {
-    var input_marca = document.getElementsByName('marca');
-    var input_precio = document.getElementsByName('precio');
-    input_marca[0].value = '';
-    input_precio[0].value = '';
+    formulario.precio.value = '';
+    formulario.marca.value = '';
 }
 
 function showResults(e) {
