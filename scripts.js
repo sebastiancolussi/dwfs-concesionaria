0e3ef2b9a6bac32b83b65315c2af89daaf6ee115
lautacampos <lauta.isa.campos@gmail.com>
2019-06-25 21:38:01 -0300

    Se realizo la modificacion al nombre de la funcion showResults, ademas se saco el alert y se agregaron las llamadas a otras funciones. Se agregaron las variables marca y precio. Sergio me rechazo el README :,(

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
 let formulario = document.getElementById("form-carga");
-	
+
+
+
 function limpiarFormulario() {
     // Completar
 }
 
-function showResults(e) {
+function enviarFormulario(e){
     e.preventDefault();
-	alert("Completame!");
+    //Variables 
+    var marca = document.getElementById("marca").value;
+    var precio = document.getElementById("precio").value;
+    agregarAuto(marca, precio);
+    actualizarListaDeAutos();
+    actualizarEstadisticas();
 }
 
-formulario.addEventListener('submit', showResults)
+formulario.addEventListener('submit', enviarFormulario)
