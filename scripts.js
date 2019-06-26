41297400ba1e33af6e765dff987936492650add8
Romina <bordenaveromina@gmail.com>
2019-06-25 21:39:42 -0300

    se creo el objeto Auto, array autos y la funcion agregarAuto con parametros y contenido

--- scripts.js
+++ scripts.js
@@ -9,4 +9,19 @@ function showResults(e) {
 	alert("Completame!");
 }
 
-formulario.addEventListener('submit', showResults)
+formulario.addEventListener('submit', showResults);
+
+function Auto (marca, precio){
+	this.marca=marca;
+	this.precio=precio;
+}
+var autos = [];
+
+function agregarAuto (marca,precio){
+	if(!marca == "undefined" ||  marca==null && precio=="undefined" || precio==null){
+		alert("Complete ambos parametros")
+	}else{
+		var auto = new Auto(marca,precio);
+		autos.push(auto);
+	}
+}
